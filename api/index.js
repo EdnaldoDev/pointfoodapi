229549fc518aeb5b73f6364e03760e81b1d1f32b
Ednaldo Cavalcante <148790739+EdnaldoDev@users.noreply.github.com>
2024-03-04 15:20:29 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,12 +15,17 @@ import {AuthenticateClient} from './middleweres/aunthenticateClient.js'
 
 import {newOrder} from './client/order.js'
 dotenv.config()
+
+
 const app= express()
+
+app.use(express.json())
+app.use(cors({origin:"*"))
+
 const port=3001
 const mongodbKey= process.env.MONGO_DB_KEY
 
-app.use(express.json())
-app.use(cors({origin:"https://pointfood.vercel.app/"))
+
 // app.use((req, res, next)=>{
 //   res.header('Access-Control-Allow-Origin', 'https://pointfood.vercel.app/');
 //   res.header('Access-Control-Allow-Methods', 'GET, POST');
